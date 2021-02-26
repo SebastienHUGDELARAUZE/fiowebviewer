042b99160730357650027d6c3c0ff433b1e91d6f
Sébastien Pezac <sebastien.pezac@imt-atlantique.net>
2021-02-26 19:28:13 +0100

    Adding a default configuration in run.py

--- fiowebviewer/engine/run.py
+++ fiowebviewer/engine/run.py
@@ -9,7 +9,7 @@ import os
 from logging.handlers import (
     RotatingFileHandler,
 )
-
+from os import path
 from flask import (
     Flask,
 )
@@ -21,6 +21,15 @@ ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(
 
 template_path = os.path.join(ROOT_PATH, 'templates')
 static_path = os.path.join(ROOT_PATH, 'static')
+
+env_var_name = 'FIOWEBVIEWER_SETTINGS'
+config_file ='/etc/fiowebviewer/config.cfg'
+
+if env_var_name not in os.environ and path.isfile(config_file):
+    os.environ[env_var_name] = config_file
+
+if 'FLASK_APP' not in os.environ:
+    os.environ['FLASK_APP'] = 'fiowebviewer'
 
 fio_webviewer = Flask(__name__,
                       template_folder=template_path, static_folder=static_path)
