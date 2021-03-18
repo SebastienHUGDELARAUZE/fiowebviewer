ec8f90e40e107f44ca5100c136cdcc520c28898a
SebastienHUGDELARAUZE <sebastien.hug-de-larauze@imt-atlantique.net>
2021-03-18 13:02:23 +0000

    Cleaning repo

--- fiowebviewer/__init__.py
+++
@@ -1,7 +0,0 @@
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-# Copyright 2019 The fiowebviewer Authors. All rights reserved.
-
-from fiowebviewer.engine.run import (
-    application,
-)
